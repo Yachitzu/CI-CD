6256d239e084734a32a31b51d393e1d7084bee70
yachitzu <tefamora6@gmail.com>
2024-05-30 03:59:48 +0000

    Updates

---
+++ precache-manifest.6c53d61c4c35823c4a99010cb4b65506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6571c10a504da521be3d08dfea24bb9",
+    "url": "/CI-CD/index.html"
+  },
+  {
+    "revision": "7a65a599b1fc1e27cb2f",
+    "url": "/CI-CD/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c72feb6dc286339a449b",
+    "url": "/CI-CD/static/js/2.38fec76d.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/CI-CD/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "7a65a599b1fc1e27cb2f",
+    "url": "/CI-CD/static/js/main.d0f1970f.chunk.js"
+  },
+  {
+    "revision": "77b442078c0968d69756",
+    "url": "/CI-CD/static/js/runtime~main.8748516b.js"
+  }
+]);
